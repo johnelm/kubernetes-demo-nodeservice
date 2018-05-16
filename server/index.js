24b1359270db4edd069faa20ee12f1f26019f442
John Elm <gitkraken.com@johnelm.com>
2018-05-16 02:58:44 -0700

    fail fast in production only

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import './common/env';
 import Server from './common/server';
 import routes from './routes';
 
-process.on( 'unhandledRejection', err => {
-  console.error(err.stack)
-  process.exit(1);
-});
+if (process.env.NODE_ENV === 'production') {
+  process.on( 'unhandledRejection', err => {
+    console.error(err.stack)
+    process.exit(1);
+  });
+}
 
 export default new Server()
   .router(routes)
