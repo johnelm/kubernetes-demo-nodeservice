315e359dc4b0db98b74c2dc76496888cfdc690f5
John Elm <gitkraken.com@johnelm.com>
2018-05-16 02:55:20 -0700

    log and exit(1) on uncaught exception

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ import './common/env';
 import Server from './common/server';
 import routes from './routes';
 
+process.on( 'unhandledRejection', err => {
+  console.error(err.stack)
+  process.exit(1);
+});
+
 export default new Server()
   .router(routes)
   .listen(process.env.PORT);
